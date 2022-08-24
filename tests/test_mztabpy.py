3347066379de8257bc1e4bc22e0a124320de0ef0
Yasset Perez-Riverol <ypriverol@gmail.com>
2022-08-24 16:31:49 +0100

    minor changes in the tests

--- tests/test_mztabpy.py
+++ tests/test_mztabpy.py
@@ -9,11 +9,11 @@ class TestCases(unittest.TestCase):
 
     def test_mztabpy_to_hdf5(self):
         # mztab storage
-        mztab = MzTabPy("../testdata/diatest.sdrf_openms_design_out.mztab", single_cache_size=50 * 1024 * 1024)
+        mztab = MzTabPy("testdata/diatest.sdrf_openms_design_out.mztab", single_cache_size=50 * 1024 * 1024)
         mztab.storage(to_tsv=True, to_hdf5=True)
 
         # Read HDF5 and filter
-        result = MzTabPy.loadHDF5("tests/diatest.sdrf_openms_design_out.hdf5", subtable='peptide',
+        result = MzTabPy.loadHDF5("testdata/diatest.sdrf_openms_design_out.hdf5", subtable='peptide',
                                   where={'accession': 'P68066', 'sequence': 'AANDDLLNSFWLLDSEKGEAR'})
         print(result)
 
